9963223c1f25feee928dd64c3c4184e4961f2721
RokDAce <rok.dajnko1@gmail.com>
2022-02-23 21:34:01 +0100

    Final code

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,23 +1,19 @@
-import.java.util.*
-import.java.lang.math
+import java.util.Scanner;
+import java.lang.Math;
+
+
 public class Gravitacija {
     public static void main(String[] args) {
-<<<<<<< HEAD
 		Scanner sc = new Scanner(System.in);
-		double v = sc.nextDouble;
-		double gravPospesek = 0,0;
-		double C = (6,674*(Math.pow(10, -11)));
-		double M = (5,972*(Math.pow(10, 24)));
-		double r = (6,371*(Math.pow(10, 6)));
-		
-		gravPospesek = (C*M)/(Math.pow((r+v), 2));
-		
-=======
-		
+		double nadVisina = sc.nextDouble();
 		double gravPospesek;
-		double nadVisina;
-        System.out.println(gravPospesek, nadVisina);
->>>>>>> izpis
+		double C = (6.674*(Math.pow(10, -11)));
+		double M = (5.972*(Math.pow(10, 24)));
+		double r = (6.371*(Math.pow(10, 6)));
+		
+		gravPospesek = (C*M)/(Math.pow((r+ nadVisina), 2));
+        System.out.println(gravPospesek);
+		System.out.println(nadVisina);
     }
 	
 }
